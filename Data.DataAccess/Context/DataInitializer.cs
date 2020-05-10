c50b24d3785f5c6ffd4eb6c35988363b25f97327
Omid Nasiri <omid_nasiriii@outlook.com>
2020-05-10 21:42:24 +0430

    Update db context #14

--- Data.DataAccess/Context/DataInitializer.cs
+++ Data.DataAccess/Context/DataInitializer.cs
@@ -3,6 +3,8 @@ using System;
 using System.Security.Cryptography;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using Cross.Abstractions.EntityEnums;
 
 namespace Data.DataAccess.Context
 {
@@ -11,6 +13,32 @@ namespace Data.DataAccess.Context
     {
         public static void Seed(this ModelBuilder modelBuilder)
         {
+            var role = new List<Role>
+            {
+                new Role
+                {
+                    Id = 1,
+                    Name = RoleType.Admin.ToString()
+                },
+                new Role
+                {
+                    Id = 2,
+                    Name = RoleType.Merchant.ToString()
+                },
+                new Role
+                {
+                    Id = 3,
+                    Name = RoleType.Transporter.ToString()
+                },
+                new Role
+                {
+                    Id = 4,
+                    Name = RoleType.DeveloperSupport.ToString()
+                }
+            };
+
+            modelBuilder.Entity<Role>().HasData(role);
+
             var salt = new byte[32];
             var iterations = new Random().Next(1000, 100000);
             byte[] hashByteArray;
@@ -38,19 +66,26 @@ namespace Data.DataAccess.Context
             var developerUser = new User
             {
                 Id = 1,
-                FirstName = "شرکت",
-                LastName = "کندو",
-                Username = "developersupport",
+                Name = "Developer",
                 Password = hashPassword,
                 CreateDateTime = now,
                 IsEnabled = true,
                 IsDeleted = false,
-                EmailAddress= "abolfazl.sh1374@gmail.com",
+                EmailAddress = "abolfazl.sh1374@gmail.com",
                 Salt = salt,
                 IterationCount = iterations,
-                Picture = "Source/1.jpg"
+                SerialNumber = Guid.NewGuid().ToString()
             };
+
             modelBuilder.Entity<User>().HasData(developerUser);
+
+            var userRole = new UserRole
+            {
+                RoleId = 4,
+                UserId = developerUser.Id
+            };
+
+            modelBuilder.Entity<UserRole>().HasData(userRole);
         }
     }
 }
--- Data.DataAccess/Context/SqlServerDbContext.cs
+++ Data.DataAccess/Context/SqlServerDbContext.cs
@@ -14,13 +14,39 @@ namespace Data.DataAccess.Context
         }
 
         public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { set; get; }
+        public DbSet<UserRole> UserRoles { get; set; }
+        public DbSet<UserToken> UserTokens { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<User>().HasIndex(u => u.FirstName);
-            modelBuilder.Entity<User>().HasIndex(u => u.LastName);
-            modelBuilder.Entity<User>().HasIndex(u => u.Username).IsUnique();
-            modelBuilder.Entity<User>().HasIndex(u => u.EmailAddress).IsUnique();
+            //modelBuilder.Entity<User>().HasIndex(u => u.Name);
+            //modelBuilder.Entity<User>().HasIndex(u => u.EmailAddress).IsUnique();
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasIndex(e => e.Name);
+                entity.HasIndex(e => e.EmailAddress).IsUnique();
+                entity.Property(e => e.Password).IsRequired();
+                entity.Property(e => e.SerialNumber).HasMaxLength(450);
+            });
+
+            modelBuilder.Entity<Role>(entity =>
+            {
+                entity.Property(e => e.Name).HasMaxLength(450).IsRequired();
+                entity.HasIndex(e => e.Name).IsUnique();
+            });
+
+            modelBuilder.Entity<UserRole>(entity =>
+            {
+                entity.HasKey(e => new { e.UserId, e.RoleId });
+                entity.HasIndex(e => e.UserId);
+                entity.HasIndex(e => e.RoleId);
+                entity.Property(e => e.UserId);
+                entity.Property(e => e.RoleId);
+                //entity.HasOne(d => d.Role).WithMany(p => p.UserRoles).HasForeignKey(d => d.RoleId);
+                //entity.HasOne(d => d.User).WithMany(p => p.UserRoles).HasForeignKey(d => d.UserId);
+            });
 
             modelBuilder.Seed();
             base.OnModelCreating(modelBuilder);
