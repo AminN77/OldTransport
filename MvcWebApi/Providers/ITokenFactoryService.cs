a7460d948708df9d65b4921f0d50ef89553ee6f5
Omid Nasiri <omid_nasiriii@outlook.com>
2020-05-11 18:51:52 +0430

    Add token factory service in pl #14

---
+++ MvcWebApi/Providers/ITokenFactoryService.cs
@@ -0,0 +1,15 @@
+﻿using MvcWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ViewModels;
+
+namespace MvcWebApi.Providers
+{
+    public interface ITokenFactoryService
+    {
+        Task<JwtTokensData> CreateJwtTokensAsync(UserSignInViewModel user);
+        string GetRefreshTokenSerial(string refreshTokenValue);
+    }
+}
