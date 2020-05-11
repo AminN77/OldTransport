4a1c5b60a72c6fe9c3b25b1ad19adf25b6f768f1
Omid Nasiri <omid_nasiriii@outlook.com>
2020-05-11 18:36:25 +0430

    Add business logic user token manager #14

---
+++ BusinessLogic.Abstractions/IBusinessLogicUserTokenManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ViewModels;
+
+namespace BusinessLogic.Abstractions
+{
+    public interface IBusinessLogicUserTokenManager
+    {
+        //Task<IBusinessLogicResult<AddUserTokenViewModel>> AddUserTokenAsync(AddUserTokenViewModel addTokenViewModel, int adderUserId);
+        Task<IBusinessLogicResult<AddUserTokenViewModel>> AddUserTokenAsync(AddUserTokenViewModel addTokenViewModel, int adderUserId);
+        Task<IBusinessLogicResult> DeleteExpiredTokensAsync();
+        Task<IBusinessLogicResult> DeleteTokenAsync(string refreshTokenId);
+        Task<IBusinessLogicResult> DeleteTokensWithSameRefreshTokenSourceAsync(string refreshTokenIdHashSource);
+        Task<IBusinessLogicResult<ListUserTokenViewModel>> FindTokenAsync(string refreshTokenId);
+        Task<IBusinessLogicResult> InvalidateUserTokensAsync(int userId);
+        Task<IBusinessLogicResult> IsValidTokenAsync(string accessToken, int userId);
+    }
+}
