925acb88854c6843efc23fec59ac6fa5dcff52d0
Omid Nasiri <omid_nasiriii@outlook.com>
2020-06-13 17:48:30 +0430

    Add users list action #90

---
+++ MvcWebApi/Controllers/AdminController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BusinessLogic.Abstractions;
+using Cross.Abstractions.EntityEnums;
+using Data.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using MvcWebApi.Provider;
+using MvcWebApi.Providers;
+using ViewModels;
+
+namespace MvcWebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [EnableCors("CorsPolicy")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly IBusinessLogicUserManager _businessLogicUserManager;
+
+        public AdminController(IBusinessLogicUserManager businessLogicUserManager)
+        {
+            _businessLogicUserManager = businessLogicUserManager;
+        }
+
+        [HttpGet]
+        // [Authorize/*(Policy = "Admin")*/]
+        [IgnoreAntiforgeryToken]
+        public async Task<IActionResult> UsersList(int page, int pageSize, string search, string sort, string filter)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+            var res = await _businessLogicUserManager.GetUsersAsync(1, page, pageSize, search, sort, filter);
+            if (!res.Succeeded) return StatusCode(500, res);
+            return Ok(res);
+        }
+    }
+}
