6761cb20f4faf43238528b441e65a6401e47fcdd
Omid Nasiri <omid_nasiriii@outlook.com>
2020-06-13 20:50:53 +0430

    Add UserProfie controller #101

---
+++ MvcWebApi/Controllers/UserProfileController.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using BusinessLogic.Abstractions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using MvcWebApi.Provider;
+
+namespace MvcWebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [EnableCors("CorsPolicy")]
+    [ApiController]
+    public class UserProfileController : ControllerBase
+    {
+        private readonly IBusinessLogicUserManager _businessLogicUserManager;
+
+        public UserProfileController(IBusinessLogicUserManager businessLogicUserManager)
+        {
+            _businessLogicUserManager = businessLogicUserManager;
+        }
+
+        [HttpGet]
+        [Authorize]
+        [IgnoreAntiforgeryToken]
+        public async Task<IActionResult> UserDetails(int userId)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+            var getterUserId = HttpContext.GetCurrentUserId();
+            var res = await _businessLogicUserManager.GetUserDetailsAsync(userId, getterUserId);
+            if (!res.Succeeded) return StatusCode(500, res);
+            return Ok(res);
+        }
+    }
+}
