e2a4f6afc3bc8f77c57d574480051064d1d4e0b2
Omid Nasiri <omid_nasiriii@outlook.com>
2020-06-14 14:06:05 +0430

    Fix if statement #100

--- BusinessLogic/BusinessLogicUserManager.cs
+++ BusinessLogic/BusinessLogicUserManager.cs
@@ -615,7 +615,7 @@ namespace BusinessLogic
                 {
                     var userRole = await _roleRepository.DeferredSelectAll().SingleOrDefaultAsync(role => role.Name == RoleTypes.User.ToString());
                     var isUserAuthorized = _userRoleRepository.DeferredSelectAll().Any(u => u.UserId == getterUserId && u.RoleId != userRole.Id);
-                    if (userId != getterUserId || isUserAuthorized)
+                    if (userId != getterUserId && !isUserAuthorized)
                     {
                         messages.Add(new BusinessLogicMessage(type: MessageType.Error, message: MessageId.AccessDenied));
                         return new BusinessLogicResult<DetailUserViewModel>(succeeded: false, result: null,
@@ -1246,7 +1246,7 @@ namespace BusinessLogic
                 {
                     var userRole = await _roleRepository.DeferredSelectAll().SingleOrDefaultAsync(role => role.Name == RoleTypes.User.ToString());
                     var isUserAuthorized = _userRoleRepository.DeferredSelectAll().Any(u => u.UserId == deactivatorUserId && u.RoleId != userRole.Id);
-                    if (userId != deactivatorUserId || isUserAuthorized)
+                    if (userId != getterUserId && !isUserAuthorized)
                     {
                         messages.Add(new BusinessLogicMessage(type: MessageType.Error, message: MessageId.AccessDenied));
                         return new BusinessLogicResult<DetailUserViewModel>(succeeded: false, result: null,
@@ -1326,7 +1326,7 @@ namespace BusinessLogic
                 {
                     var userRole = await _roleRepository.DeferredSelectAll().SingleOrDefaultAsync(role => role.Name == RoleTypes.User.ToString());
                     var isUserAuthorized = _userRoleRepository.DeferredSelectAll().Any(u => u.UserId == activatorUserId && u.RoleId != userRole.Id);
-                    if (userId != activatorUserId || isUserAuthorized)
+                    if (userId != getterUserId && !isUserAuthorized)
                     {
                         messages.Add(new BusinessLogicMessage(type: MessageType.Error, message: MessageId.AccessDenied));
                         return new BusinessLogicResult<DetailUserViewModel>(succeeded: false, result: null,
